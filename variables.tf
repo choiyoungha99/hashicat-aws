f196ed0fb38db836c81f2b1d37f917caa932fb9d
choiyoungha99 <163215068+choiyoungha99@users.noreply.github.com>
2024-03-19 16:59:03 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -49,7 +49,3 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-variable "bucket_prefix" {
-  default     = "s3"
-  Description = "(Optional, Forces new resource) Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
-}
